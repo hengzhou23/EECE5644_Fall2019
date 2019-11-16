c90e971686eb5dc84e5ea3e010bf606813003465
harryzhou23 <heng.zhou23@gmail.com>
2019-11-15 19:11:11 -0500

    Update image_generator.m

--- Homework_4/Q1/image_generator.m
+++ Homework_4/Q1/image_generator.m
@@ -1,17 +1,17 @@
-function [image, x_vals, y_vals] = image_generator(data, img_width, img_height)
-    color_list = [[0.25,0.25,0.5];
-                  [0.9,0.1,0];
-                  [0.2,0.1,0.7];
-                  [0.4,0.2,0.4];
-                  [0,2,1];];
+function [image, x_vals, y_vals] = image_generator(data, width, height)
+    C = [[0.25,0.25,0.5];
+         [0.9,0.1,0];
+         [0.2,0.1,0.7];
+         [0.4,0.2,0.4];
+         [0,2,1];];
     image = zeros(img_height, img_width, 3);
 
-    x_vals = rescale(data.X, 1, img_width);
-    y_vals = rescale(data.Y, 1, img_height);
+    x_vals = rescale(data.X, 1, width);
+    y_vals = rescale(data.Y, 1, height);
 
-    T_len = length(data.X);
+    len = length(data.X);
 
-    for i = 1:T_len
-        image(int16(y_vals(i)), int16(x_vals(i)), :) = color_list(int8(data.Class(i)), :);
+    for i = 1:len
+        image(int16(y_vals(i)), int16(x_vals(i)), :) = C(int8(data.Class(i)), :);
     end
 end
