a2d1aee784f780e94da3fc18962d6a23eb86e02a
harryzhou23 <heng.zhou23@gmail.com>
2019-11-15 19:03:57 -0500

    update

--- Homework_4/Q1/gmm.m
+++ Homework_4/Q1/gmm.m
@@ -1,10 +1,5 @@
 function [gmm_data, gm_model] = gmm(data, cluster_num)
-    fprintf("GMM with %d clusters\n", cluster_num)
-
-    gm_model = fitgmdist(data, cluster_num,...
-            'Replicates', 4,...
-            'Options', statset('MaxIter',500,'TolFun',1e-6));
-
+    gm_model = fitgmdist(data, cluster_num,'Replicates', 4, 'Options', statset('MaxIter',500,'TolFun',1e-6));
     class = gm_model.cluster(data);
     gmm_data = array2table([data class], 'VariableNames', {'X', 'Y', 'R', 'G', 'B', 'Class'});
 end
--- Homework_4/Q1/k_means.m
+++ Homework_4/Q1/k_means.m
@@ -1,8 +1,4 @@
 function [kmeans_data, C, sumD, D] = k_means(data, cluster_num)
-    fprintf("K-Means with %d clusters\n", cluster_num);
-
-    [idx,C,sumD,D] = kmeans(data, cluster_num, 'Distance','sqeuclidean',...
-        'Replicates', 5, 'MaxIter', 500, 'Start', 'cluster');
-
+    [idx,C,sumD,D] = kmeans(data, cluster_num, 'Distance','sqeuclidean', 'Replicates', 5, 'MaxIter', 500, 'Start', 'cluster');
     kmeans_data = array2table([data idx], 'VariableNames', {'X', 'Y', 'R', 'G', 'B', 'Class'});
 end    
