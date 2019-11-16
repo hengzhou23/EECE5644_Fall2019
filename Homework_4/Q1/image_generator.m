5f926fab045424629730f96912c3a56928355a70
harryzhou23 <heng.zhou23@gmail.com>
2019-11-15 19:15:19 -0500

    update

--- Homework_4/Q1/image_generator.m
+++ Homework_4/Q1/image_generator.m
@@ -6,12 +6,11 @@ function [image, x_vals, y_vals] = image_generator(data, width, height)
          [0,2,1];];
     image = zeros(img_height, img_width, 3);
 
-    x_vals = rescale(data.X, 1, width);
-    y_vals = rescale(data.Y, 1, height);
-
+    x = rescale(data.X, 1, width);
+    y = rescale(data.Y, 1, height);
     len = length(data.X);
 
     for i = 1:len
-        image(int16(y_vals(i)), int16(x_vals(i)), :) = C(int8(data.Class(i)), :);
+        image(int16(y(i)), int16(x(i)), :) = C(int8(data.Class(i)), :);
     end
 end
