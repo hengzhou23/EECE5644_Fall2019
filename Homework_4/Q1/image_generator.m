a09c3540f36a8cdc595d67dbcfebf6c1f7d126f9
harryzhou23 <heng.zhou23@gmail.com>
2019-11-15 19:17:28 -0500

    Update image_generator.m

--- Homework_4/Q1/image_generator.m
+++ Homework_4/Q1/image_generator.m
@@ -1,10 +1,10 @@
-function [image, x_vals, y_vals] = image_generator(data, width, height)
+function [image, x, y] = image_generator(data, width, height)
     C = [[0.25,0.25,0.5];
          [0.9,0.1,0];
          [0.2,0.1,0.7];
          [0.4,0.2,0.4];
          [0,2,1];];
-    image = zeros(img_height, img_width, 3);
+    image = zeros(height, width, 3);
 
     x = rescale(data.X, 1, width);
     y = rescale(data.Y, 1, height);
