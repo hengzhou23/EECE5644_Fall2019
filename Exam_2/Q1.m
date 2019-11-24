fcdee2e728b403e9a0b8e44937d949715dcbf4a0
harryzhou23 <heng.zhou23@gmail.com>
2019-11-24 16:30:23 -0500

    Update Q1.m

--- Exam_2/Q1.m
+++ Exam_2/Q1.m
@@ -109,6 +109,10 @@ for i = 1:level
     % Update weights
     w = w .* exp(a .* inequality);    % compute un-normalized weights
     w = w / sum(w);                 % renormalize the weights
+    
+    if any(w<=0)
+        w = initial_weight;
+    end
 end
 final_weight = w;
 initial_final_W = [initial_weight, final_weight];
